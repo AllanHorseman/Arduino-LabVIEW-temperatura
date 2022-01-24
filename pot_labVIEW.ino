e0a9c132917b991a46efec811e165b4ea16fdf87
AllanHorseman <87746495+AllanHorseman@users.noreply.github.com>
2022-01-24 02:18:40 -0600

    Add files via upload

--- pot_labVIEW.ino
+++ pot_labVIEW.ino
@@ -4,7 +4,8 @@
 const int rs = 9, en = 8, d4 = 7, d5 = 6, d6 = 5, d7 = 4, ref=50, periodo = 500;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int pot, var, relay = 13, ahora = 0;
+int pot, var, relay = 13;
+unsigned long ahora = 0;
 float temp;
 
 void setup() {
@@ -31,11 +32,31 @@ void loop() {
     if(var=='b')
       {digitalWrite(relay,LOW);}
 
-    lcd.setCursor(0,0);       //Volver al inicio 
+/////////El LCD muestra la temperatura sin considerar la temperatura de referencia/////////
+    /*lcd.setCursor(0,0);       //Volver al inicio 
     lcd.print("Temp Ref: ");     //Muestra en LCD Temperatura//
     lcd.print (ref);
     lcd.setCursor(0,1);        // Cambia de renglon en el LCD//
     lcd.print("Temp:");     //Muestra en LCD Temperatura//
-    lcd.print(temp);
+    lcd.print(temp);*/
+///////////////////////////////////////////////////////////////////////////////////////////
+    
+    if (temp >= ref){                  // Para una temperatura alta//
+      lcd.setCursor(0,0);
+      lcd.print("Temp Ref: ");     //Muestra en LCD Temperatura//
+      lcd.print (ref);
+      lcd.setCursor (0,1);        // Cambia de renglon en el LCD//
+      lcd.print("Temp alta:");     //Muestra en LCD Temperatura//
+      lcd.print(temp);           //muestra la variable de la temperatura//
+    }
+    
+    else{
+      lcd.setCursor(0,0);
+      lcd.print("Temp Ref:");     //Muestra en LCD Temperatura//
+      lcd.print (ref);
+      lcd.setCursor (0,1);        // Cambia de renglon en el LCD//
+      lcd.print ("Temp baja:");         //Imprime Alta en el segundo renglon//
+      lcd.print (temp);           //muestra la variable de la temperatura//
+    }
   }
 }
