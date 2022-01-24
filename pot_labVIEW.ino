fea6fa5fc47259c7b87244ad77a0d90574067ae4
AllanHorseman <87746495+AllanHorseman@users.noreply.github.com>
2022-01-24 00:58:24 -0600

    Add files via upload

--- pot_labVIEW.ino
+++ pot_labVIEW.ino
@@ -1,3 +1,9 @@
+#include <LiquidCrystal.h>
+
+//Declaramos los pines del LCD//
+const int rs = 9, en = 8, d4 = 7, d5 = 6, d6 = 5, d7 = 4, ref=50;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 int pot, var;
 int relay = 13; 
 float temp;
