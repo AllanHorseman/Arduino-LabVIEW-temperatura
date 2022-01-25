b41c9f4c63bdd8f6bdc390d10c7e2c89d794f0f6
AllanHorseman <87746495+AllanHorseman@users.noreply.github.com>
2022-01-25 00:08:58 -0600

    Add files via upload

--- Keyboard_custome.ino
+++ Keyboard_custome.ino
@@ -13,13 +13,14 @@ const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 char wawa[7];
 int i = 0;
+float ref;
 
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'.','0','-','D'}
 };
 byte rowPins[ROWS] = {11, 10, 9, 8}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
@@ -35,15 +36,29 @@ void loop(){
   char customKey = customKeypad.getKey();
   
   if (customKey){
-    wawa[i] = customKey;
-    Serial.println(wawa);
-    i++;
+    if(customKey != 'A'){
+      wawa[i] = customKey;
+      Serial.println(wawa);
+      i++;
+    }
+  }
 
-    if(i==6){
-      i = 0;
-      for(int j = 0; j<7; j++){
-        wawa[j] = '\0';        
-      }
+  if(i == 6 or customKey == 'A'){
+    i = 0;
+    ref = atof(wawa);
+
+    if(ref<-50.00 or ref>150.00){
+      Serial.println("Fuera de escala!");
+    }
+
+    else{
+      Serial.print("Nuevo setpoint: ");
+      Serial.println(ref);
+      //Serial.println(ref); 
+    }
+    
+    for(int j = 0; j<7; j++){
+      wawa[j] = '\0';        
     }
   }
 }
